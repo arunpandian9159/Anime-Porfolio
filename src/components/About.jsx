74124c59757cbb1ed8c174ba4df73257d74061a7
arunpandian9159 <arunpandiancse25@gmail.com>
2026-01-28 19:21:23 +0530

    feat: update About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ import { formatBoldText } from "../utils/formatText";
 const StatCard = memo(({ icon, value, label, index }) => {
   const countRef = useCountUp(value); 
   const cardRef = useCardReveal(index, { translateY: [20, 0] });  
-
+ 
   return (
     <div
       ref={cardRef}
